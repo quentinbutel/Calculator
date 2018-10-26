cf5dc5d0dd60a956a2ba4db9d18580128e0e341c
quentinbutel <44508495+quentinbutel@users.noreply.github.com>
2018-10-26 22:13:27 +0200

    Compute addition of two intergers

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition (int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+   printf("%d\n", addition(1, 4));
+   return 0;
 }
