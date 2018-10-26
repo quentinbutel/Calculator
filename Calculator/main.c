22518fa3dfe592224b8c60a713d42f34eda70498
quentinbutel <44508495+quentinbutel@users.noreply.github.com>
2018-10-26 22:29:17 +0200

    add soustraction fonction

--- Calculator/main.c
+++ Calculator/main.c
@@ -5,11 +5,16 @@ int addition (int a, int b)
 {
     return a + b;
 }
+int soustraction (int a, int b)
+{
+    return a - b;
+}
 
 int main()
 {
 
    printf("L'addition de a et de b vaut %d\n", addition(1, 4));
+   printf("%d\n", soustraction(1, 4);)
 
    return 0;
 }
